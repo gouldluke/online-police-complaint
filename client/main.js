02a67c39ba5ea2107eb103e3b47da2ce694c8802
Luke Gould <lukegould@Lukes-MacBook-Pro.local>
2018-01-25 20:31:29 -0500

    remove meteor project and create new npm project, add some client side dependencies with bower

--- client/main.js
+++
@@ -1,8 +0,0 @@
-import angular from 'angular';
-import angularMeteor from 'angular-meteor';
-import complaintForm from '../imports/components/complaintForm/complaintForm';
-
-angular.module('online-police-complaint', [
-  angularMeteor,
-  complaintForm.name
-]);
