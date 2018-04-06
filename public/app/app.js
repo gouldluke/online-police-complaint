0e3e431c5fdbd04c5cd4a5d26d71d0aa5e7615d5
Luke Gould <lukegould@Lukes-MacBook-Pro.local>
2018-04-05 20:26:10 -0500

    reformat view and add some responsive action to submitting the form

--- public/app/app.js
+++ public/app/app.js
@@ -7,9 +7,34 @@ angular.module('app').config(function ($routeProvider, $locationProvider){
 });
 
 angular.module('app').controller('mainCtrl', function ($scope) {
-  $scope.myVar = "City of Euclid";
+  $scope.showForm = false;
+  $scope.showNextSteps = false;
 
-  $scope.submitReport = function () {
-    $scope.dataToSubmit = $scope.formData;
+  $scope.city = "City of Euclid";
+  $scope.introductionText = [
+      "If you have experienced violence, brutality or oppression at the hands " +
+      "of members of the police department, please consider completing the form below to report this " +
+      "violation.",
+      "If you would like to remain anonymous, please do not include your name. We may not " +
+      "be able to submit this report to the department, but your contribution will still enable us to " +
+      "collect data on the department to present to city officials."
+  ];
+  $scope.differenceExplanation = "You can either submit your complaint directly with the police department or"
+  $scope.completedText = "Thank you for submitting the report and helping keep police accountable in Ohio! Your report ID number is *** xxxxxxx ***. You will recieve an email and/or a text with this ID number. You can reach out to us at xxx@xxx.com or 440-xxx-xxxx if you would like any further information about the status of your report, but we will do our best to contact you if we find out any further information. Please visit euclidpolicecomplaint.com/xxx to view statistics on reports from you police department and get in touch with other victims of police violence."
+
+  $scope.falsificationUrl = "http://codes.ohio.gov/orc/2921.13"
+
+  $scope.submitViaWebsite = function () {
+      $scope.showForm = true;
+  }
+
+  $scope.submitViaDepartment = function () {
+      $scope.showForm = false;
+  }
+
+  $scope.submitForm = function () {
+      $scope.dataToSubmit = $scope.formData;
+      $scope.showForm = false;
+      $scope.showNextSteps = true;
   };
 })
---
+++ purpose.feature
@@ -0,0 +1,12 @@
+As a victim of police brutality
+I would like the ability to report the offense
+So that officers will be held accountable for their actions
+
+Scenario: An officer has commit an illegal traffic stop
+    Given   A citizen is pulled over by police
+    And     The officer arrests the individual after they refused an illegal search of their vehicle
+    When    The individual submits an online complaint
+    Then    The department should initiate an investigation with the officer in question
+    And     Take into account other online complaints and other information
+    And     Communicate with victims of the officer and believe their testimony
+    And     Take significant action to correct the officers behavior
