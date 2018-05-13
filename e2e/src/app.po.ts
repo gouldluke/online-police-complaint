21187d29fec951f6f51f8bd20f09ee491ac87f65
Luke Gould <lukegould@Lukes-MacBook-Pro.local>
2018-05-12 23:45:05 -0400

    update from angualr 2 to angular 6

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,6 +1,6 @@
-import { browser, element, by } from 'protractor';
+import { browser, by, element } from 'protractor';
 
-export class OnlinePoliceComplaintPage {
+export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
